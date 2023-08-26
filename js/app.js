f2cfcc20e1d3fe8f4a7d6416de66659964ee84a0
amirhossein allami <captain2480722023@gmail.com>
2023-08-27 02:25:41 +0330

    add transition for modal

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ const closeBtn = $.querySelector('.close-button')
 // rules button logic
 rulesBtn.addEventListener('click', () => {
     modalElem.style.display = 'flex'
-    modalElem.style.opacity = '1'
-    modalElem.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'
+    setTimeout(() => {
+        modalElem.style.opacity = '1'
+        modalElem.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'
+    }, 0)
 })
 closeBtn.addEventListener('click', () => {
     modalElem.style.display = 'none'
