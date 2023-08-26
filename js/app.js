9d9dd363bd62d3186ec03c1d465cf01dcff1641f
amirhossein allami <captain2480722023@gmail.com>
2023-08-27 02:09:18 +0330

    update

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let $ = document
+
+const rulesBtn = $.querySelector('.rules')
+const modalElem = $.querySelector('.modal')
+const closeBtn = $.querySelector('.close-button')
+
+rulesBtn.addEventListener('click', () => {
+    modalElem.style.display = 'flex'
+    modalElem.style.opacity = '1'
+    modalElem.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'
+})
+closeBtn.addEventListener('click', () => {
+    modalElem.style.display = 'none'
+    modalElem.style.opacity = '0'
+    modalElem.style.backgroundColor = 'transparent'
+})
